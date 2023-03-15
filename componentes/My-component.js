b6e4c16d1e6702efa6ee2e64a9c313d6aedf1b9b
MishellDallanaRoblesSerenoADSO <Mishellrobles2004@gmail.com>
2023-03-15 11:43:12 -0500

    feat: :rocket: Directivas
    Dinamico

--- componentes/My-component.js
+++ componentes/My-component.js
@@ -1,9 +1,10 @@
 export default{
     data(){
         return {
+            atributo: "src",
             imagen: "https://lenguajejs.com/vuejs/componentes/composition-api/options-api-composition-api.png",
+            atributo2: "alt",
             texto: "Diferencias entre vue2 y vue3"
-            
         }
     }
 }
