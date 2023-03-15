ca1949a4ed64ebdc44d5edf87b9cc0b80324e28d
MishellDallanaRoblesSerenoADSO <Mishellrobles2004@gmail.com>
2023-03-15 16:27:08 -0500

    feat: :rocket: Directivas
    Style

--- componentes/My-component.js
+++ componentes/My-component.js
@@ -1,13 +1,17 @@
 export default{
     data(){
         return {
-            className: "box",
-            className2: ["box", "box2"],
-            className3: {
-                box:true,
-                box2: false,
-                box3:true
-            }
+           styleName: "background: red",
+           styleName2: ["background: blueviolet", "color: #fff"],
+           styleName3: {
+                background: "violet",
+                color: "#fff", 
+                "font-size": "2rem",
+                "font-family": "sans-serif",
+                padding: "1em",
+                "border-radius": "2rem",
+                margin: ".5rem 0"
+           }
         }
     }
 }
