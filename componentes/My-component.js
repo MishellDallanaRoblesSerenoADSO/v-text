000e27660b49731b834ac44c176e560e232079fe
MishellDallanaRoblesSerenoADSO <Mishellrobles2004@gmail.com>
2023-03-15 11:29:07 -0500

    feat: :rocket: Directivas
    v-for

--- componentes/My-component.js
+++ componentes/My-component.js
@@ -1,12 +1,12 @@
 export default{
     data(){
         return {
-            mensaje: `<em>Apolo</em>`,
-            mensaje2: `<mark>Artemis</mark>`,
-            mensaje3: `<u>Sputnik</u>`,
-            mensaje4: `NO CUMPLE LA CONDICION`,
-            opcion: null,
+            lista: [
+                "Apolo",
+                "Artemis",
+                "Sputnik"
 
+            ]
         }
     }
 }
