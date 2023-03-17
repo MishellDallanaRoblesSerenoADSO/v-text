a055f46b23817882a0b1064013b20e4612008855
MishellDallanaRoblesSerenoADSO <Mishellrobles2004@gmail.com>
2023-03-17 10:15:36 -0500

    feat: :rocket: Directivas
    v-on example

--- componentes/My-component.js
+++ componentes/My-component.js
@@ -1,17 +1,12 @@
 export default{
     data(){
         return {
-           styleName: "background: red",
-           styleName2: ["background: blueviolet", "color: #fff"],
-           styleName3: {
-                background: "violet",
-                color: "#fff", 
-                "font-size": "2rem",
-                "font-family": "sans-serif",
-                padding: "1em",
-                "border-radius": "2rem",
-                margin: ".5rem 0"
-           }
+           event: "click"
+        }
+    },
+    methods: {
+        showMessage() {
+            alert("Hola care bola")
         }
     }
 }
